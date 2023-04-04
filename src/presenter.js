35436381b12e23c54f267f7619db06c24661823a
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-04-04 07:50:33 -0400

    Implementaciond de cadenas

--- src/presenter.js
+++ src/presenter.js
@@ -1,8 +1,8 @@
-import sumar from "./sumador";
+import controladorAuto from "./ControladorAuto";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const first = document.querySelector("#primera-coordenada");
+const second = document.querySelector("#segunda-coordenada");
+const form = document.querySelector("#matriz-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
@@ -11,5 +11,5 @@ form.addEventListener("submit", (event) => {
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + controladorAuto + "</p>";
 });
